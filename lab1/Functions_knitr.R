e116e30bc8d9247bd442e4bae746f9b9a08861d8
Lihe Liu <lihe.liu@ufl.edu>
2019-09-11 00:42:26 -0400

    lab2

---
+++ lab1/Functions_knitr.R
@@ -0,0 +1,20 @@
+# ---- check_package ----
+options(warn = -1,repos="https://cran.rstudio.com")
+install.packages("easypackasges",quite = T);library(easypackages)
+Check_pkg = function(mypkg){
+  miss = logical()
+  for (i in seq_along(mypkg)){
+    test = is.element(mypkg[i],installed.packages()[,1])
+    if (!test){miss[i] = T} else {miss[i] = F}
+  }
+  miss_pkg = mypkg[miss]
+  if (!length(miss_pkg) == 0){
+    message("Now checking package requirements...\nOops! You are missing ",length(miss_pkg)," package(s), and they are: ",miss_pkg)
+    for (j in seq_along(miss_pkg)){
+      message("Now installing missing package number",j, " -", miss_pkg[j])
+      install.packages(miss_pkg[j],repos = "https://cran.rstudio.com", quiet = T)
+    }
+  } else {message("Packages requirements checked! You are all set!")}
+}
+Check_pkg(mypkg)
+libraries(mypkg)
